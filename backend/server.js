5589b0999c135383569b6a9f421145d6eff65f86
Aisi040 <aiserich.engblom@gritacademy.se>
2023-10-07 10:59:48 +0200

    Backend delen

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+// Middleware för att hantera JSON-data
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Sten, Sax, Påse backend!');
+});
+
+// Hämta highscore-listan
+app.get('/highscore', (req, res) => {
+    const highscores = JSON.parse(fs.readFileSync('highscore.json', 'utf8'));
+    res.json(highscores);
+});
+
+// Uppdatera highscore-listan
+app.post('/highscore', (req, res) => {
+    const newScore = req.body;
+    let highscores = JSON.parse(fs.readFileSync('highscore.json', 'utf8'));
+
+    // Lägg till den nya poängen och sortera listan
+    highscores.push(newScore);
+    highscores = highscores.sort((a, b) => b.score - a.score).slice(0, 5);
+
+    fs.writeFileSync('highscore.json', JSON.stringify(highscores, null, 2));
+    res.json({ message: 'Highscore updated!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
